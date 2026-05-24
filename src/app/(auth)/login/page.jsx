8dd85b952fe9a4d6d1c79cc0902c3fbcbf4218a7
sakibulislam75 <sakibulislam1623@gmail.com>
2026-05-24 06:33:54 +0600

    chore: initialize project structure with route groups

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogInPage = () => {
+   return (
+      <div>
+         <h1>LogIn</h1>
+      </div>
+   );
+};
+
+export default LogInPage;
---
+++ src/app/(main)/news/[...newsId]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsDetailsPage = () => {
+   return (
+      <div>
+         <h1 className="text-yellow-600">NewsDetails</h1>
+      </div>
+   );
+};
+
+export default NewsDetailsPage;
